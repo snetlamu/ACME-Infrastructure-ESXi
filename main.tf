7c606b4dced85fa5b77406cfb6072b0ba01aa16e
Suraj <snetlamu@cisco.com>
2024-12-06 16:15:51 +0530

    Fixed reference

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "vsphere_resource_pool" "Parent_Pool" {
 
 resource "vsphere_resource_pool" "Child_Pools" {
   for_each                = toset(var.clusters)
-  parent_resource_pool_id = vsphere_resource_pool.Infrastructure.id
+  parent_resource_pool_id = vsphere_resource_pool.Parent_Pool.id
   name                    = "${var.prefix}-${each.value}"
 }

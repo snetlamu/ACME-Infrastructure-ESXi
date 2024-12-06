14cc3863026205cf1a1e21e7335c29109dbfcb50
Suraj <snetlamu@cisco.com>
2024-12-06 15:52:07 +0530

    Fixed for_each

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "vsphere_compute_cluster" "Infrastructure" {
 }
 
 resource "vsphere_compute_cluster" "Management" {
-  for_each = var.clusters
+  for_each = toset(var.clusters)
   datacenter_id = data.vsphere_datacenter.datacenter.id
   host_system_ids = [ vsphere_compute_cluster.Infrastructure.id ]
   name = "${var.prefix}-${each.value}"
